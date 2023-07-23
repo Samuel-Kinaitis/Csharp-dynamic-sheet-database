c653fab341b44dd8c6fbc5ba5faa544b9b69ae8d
Sam <samuel.kinaitis@gmail.com>
2023-07-23 15:47:48 -0400

    upload

---
+++ FinalProjectSamuelKinaitisCSCI234/FinalProjectSamuelKinaitisCSCI234/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace FinalProjectSamuelKinaitisCSCI234
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.aDDUIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aDDDTMDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cHGUIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cHGDTMDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sTATCDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database3DataSet = new FinalProjectSamuelKinaitisCSCI234.Database3DataSet();
+            this.tableTableAdapter = new FinalProjectSamuelKinaitisCSCI234.Database3DataSetTableAdapters.TableTableAdapter();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undeleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.purgeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmdExit = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database3DataSet)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.aDDUIDDataGridViewTextBoxColumn,
+            this.aDDDTMDataGridViewTextBoxColumn,
+            this.cHGUIDDataGridViewTextBoxColumn,
+            this.cHGDTMDataGridViewTextBoxColumn,
+            this.sTATCDDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 27);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(543, 314);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // aDDUIDDataGridViewTextBoxColumn
+            // 
+            this.aDDUIDDataGridViewTextBoxColumn.DataPropertyName = "ADD_UID";
+            this.aDDUIDDataGridViewTextBoxColumn.HeaderText = "ADD_UID";
+            this.aDDUIDDataGridViewTextBoxColumn.Name = "aDDUIDDataGridViewTextBoxColumn";
+            // 
+            // aDDDTMDataGridViewTextBoxColumn
+            // 
+            this.aDDDTMDataGridViewTextBoxColumn.DataPropertyName = "ADD_DTM";
+            this.aDDDTMDataGridViewTextBoxColumn.HeaderText = "ADD_DTM";
+            this.aDDDTMDataGridViewTextBoxColumn.Name = "aDDDTMDataGridViewTextBoxColumn";
+            // 
+            // cHGUIDDataGridViewTextBoxColumn
+            // 
+            this.cHGUIDDataGridViewTextBoxColumn.DataPropertyName = "CHG_UID";
+            this.cHGUIDDataGridViewTextBoxColumn.HeaderText = "CHG_UID";
+            this.cHGUIDDataGridViewTextBoxColumn.Name = "cHGUIDDataGridViewTextBoxColumn";
+            // 
+            // cHGDTMDataGridViewTextBoxColumn
+            // 
+            this.cHGDTMDataGridViewTextBoxColumn.DataPropertyName = "CHG_DTM";
+            this.cHGDTMDataGridViewTextBoxColumn.HeaderText = "CHG_DTM";
+            this.cHGDTMDataGridViewTextBoxColumn.Name = "cHGDTMDataGridViewTextBoxColumn";
+            // 
+            // sTATCDDataGridViewTextBoxColumn
+            // 
+            this.sTATCDDataGridViewTextBoxColumn.DataPropertyName = "STAT_CD";
+            this.sTATCDDataGridViewTextBoxColumn.HeaderText = "STAT_CD";
+            this.sTATCDDataGridViewTextBoxColumn.Name = "sTATCDDataGridViewTextBoxColumn";
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.database3DataSet;
+            // 
+            // database3DataSet
+            // 
+            this.database3DataSet.DataSetName = "Database3DataSet";
+            this.database3DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(566, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.undeleteToolStripMenuItem,
+            this.purgeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // undeleteToolStripMenuItem
+            // 
+            this.undeleteToolStripMenuItem.Name = "undeleteToolStripMenuItem";
+            this.undeleteToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.undeleteToolStripMenuItem.Text = "Undelete";
+            this.undeleteToolStripMenuItem.Click += new System.EventHandler(this.undeleteToolStripMenuItem_Click);
+            // 
+            // purgeToolStripMenuItem
+            // 
+            this.purgeToolStripMenuItem.Name = "purgeToolStripMenuItem";
+            this.purgeToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.purgeToolStripMenuItem.Text = "Purge";
+            this.purgeToolStripMenuItem.Click += new System.EventHandler(this.purgeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(121, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem1});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(98, 22);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // cmdExit
+            // 
+            this.cmdExit.Location = new System.Drawing.Point(480, 371);
+            this.cmdExit.Name = "cmdExit";
+            this.cmdExit.Size = new System.Drawing.Size(75, 23);
+            this.cmdExit.TabIndex = 2;
+            this.cmdExit.Text = "Exit";
+            this.cmdExit.UseVisualStyleBackColor = true;
+            this.cmdExit.Click += new System.EventHandler(this.cmdExit_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.AccessibleName = "";
+            this.txtSearch.Location = new System.Drawing.Point(136, 371);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(97, 20);
+            this.txtSearch.TabIndex = 4;
+            this.txtSearch.Tag = "";
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 374);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Search from STAT_CD:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 407);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cmdExit);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database3DataSet)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private Database3DataSet database3DataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private Database3DataSetTableAdapters.TableTableAdapter tableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aDDUIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aDDDTMDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cHGUIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cHGDTMDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sTATCDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undeleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem purgeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Button cmdExit;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ FinalProjectSamuelKinaitisCSCI234/FinalProjectSamuelKinaitisCSCI234/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace FinalProjectSamuelKinaitisCSCI234
+{
+    public partial class Form1 : Form
+    {
+            //varibles 
+        //Keeps track of the current row the user is on
+        int index;
+        //delete log
+        int lastDeleted;
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database3DataSet.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.database3DataSet.Table);
+        }
+
+
+        //Menu - File -- Exit
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+
+        //Form -- Exit
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        //Menu - File -- New (goes to fresh row)
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.CurrentCell = dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[0];
+        }
+
+        //Menu - File -- Delete (hides selected row)
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Purge last deleted
+            dataGridView1.Rows.RemoveAt(lastDeleted);
+            //set as next deleted
+            lastDeleted = index;
+            
+            //Move to fresh row
+            dataGridView1.CurrentCell = dataGridView1.Rows[dataGridView1.RowCount - 1].Cells[0];
+            //Hide requested row
+            dataGridView1.Rows[index].Visible = false;
+        }
+
+        //Menu - File -- Undelete (unhides last hidden row)
+        private void undeleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows[lastDeleted].Visible = true;
+        }
+
+        //Keeps track of the current row the user is on
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            index = e.RowIndex;
+        }
+
+        //Menu - File -- Purge (Deletes row)
+        private void purgeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.RemoveAt(index);
+        }
+
+        //Menu - File -- Save
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        //Menu - Edit -- Save 
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            save();
+        }
+
+        //Save method (Saves DataGrid to database) 
+        private void save()
+        {
+            SqlConnection conection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database3.mdf;Integrated Security=True");
+            conection.Open();
+            SqlCommand cmd = new SqlCommand("insert into Table values (@ADD_UID,@ADD_DTM,@CHG_UID,@CHG_DTM,@STAT_CD)", conection);
+            cmd.Parameters.AddWithValue("@ADD_UID", dataGridView1.Rows[index].Cells[0].Value);
+            cmd.Parameters.AddWithValue("@ADD_DTM", dataGridView1.Rows[index].Cells[1].Value);
+            cmd.Parameters.AddWithValue("@CHG_UID", dataGridView1.Rows[index].Cells[2].Value);
+            cmd.Parameters.AddWithValue("@CHG_DTM", dataGridView1.Rows[index].Cells[3].Value);
+            cmd.Parameters.AddWithValue("@STAT_CD", dataGridView1.Rows[index].Cells[4].Value);
+            conection.Close();
+            MessageBox.Show("Saved");
+        }
+
+        //Menu - Help -- About (instructions)
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("File" +
+                "\n - New: Goes to a fresh row" +
+                "\n - Open: Does nothing" +
+                "\n - Save: Saves selected row" +
+                "\n - Delete: Removes row" +
+                "\n - Undelete: Brings last removed row" +
+                "\n - Purge: Deletes row" +
+                "\n - Exit: Closes program" +
+                "\n" +
+                "\nEdit" +
+                "\n - Save: Saves selected row" +
+                "\n" +
+                "\nHelp" +
+                "\n - About: Shows this message" +
+                "\n" +
+                "\nSearch Bar" +
+                "\nSearch from STAT_CD");
+        }
+
+        //Search Bar - Searches and bring up only the results in the data grid
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            DataTable dt = database3DataSet.Table;
+            DataView dv = dt.DefaultView;
+            dv.RowFilter = string.Format("STAT_CD like '%" + txtSearch.Text + "%'");
+            dataGridView1.DataSource = dv.ToTable();
+        }
+
+
+    }
+}
